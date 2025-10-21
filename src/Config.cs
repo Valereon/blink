d5e4f23aaf739a5ab90fb7da23305c3756d72e61
Maxwell Warren <95433166+Valereon@users.noreply.github.com>
2025-10-21 18:48:35 -0400

    updated base config toml for future things

--- src/Config.cs
+++ src/Config.cs
@@ -13,6 +13,8 @@ public static class Config
     public const string ShellExe = "shellExecutable";
     public const string ShellExtraArgs = "extraShellArgs";
 
+    public const string PathModWindows = @"set PATH=";
+
     public static string PathSeparator = @"\";
 
     // blink env stuff
@@ -32,7 +34,6 @@ public static class Config
     public const string BaseConfigTOML = """
     path = [] # NEVER SHOULD BE ABSOLUTE
     fileSystemRoot = "" # ABSOLUTE
-    targetPlatforms = []    
     
 
     # execution and fallback
@@ -40,14 +41,26 @@ public static class Config
     #SHELL: runs always in the shell 
     #ASK:  when blink run fails asks if should run in the shell
     #BLINK: runs in the blink and if it fails then throws
+    # IF NONE OF THIS ARE CHOSEN, or if one is spelled incorrectly it will automatically assume "AUTO"
     mode = "auto" # auto | shell | ask | blink     
 
     shellExecutable = "cmd.exe"
     extraShellArgs = "/c"
+
+    #Path Modification
+    # this will allow you to append the ENTIRE blink path to the front or end of your current system path.
+    # KEEP IN MIND!!!! this will only affect the current shell session none of this is permanent.
+    # If you wish to get your original path back please restart your shell
+    # THIS WILL ALSO NOT TAKE INTO ACCOUNT YOUR BUILD.TOML!
+    #
+    # 0: NONE will not append to your path at all
+    # 1: PREPEND to your path so it will "blinkPath;%PATH%"
+    # 2: POSTPEND to your path "%PATH%;blinkPath;"
     """;
 
 
     public const string BaseBuildTOML = """
+    # YOU MUST put every arg into its own string in a list, because of how the paths and stuff work this is the best way i found to do this. Also it looks nicer
     test = ["echo", "hello"]
 
 
